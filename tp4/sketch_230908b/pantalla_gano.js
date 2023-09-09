534600466c3e26fd6ee644dd4ed92de077cc655f
franciscaduart1 <129305147+franciscaduart1@users.noreply.github.com>
2023-09-08 21:04:23 -0300

    Delete tp4/sketch_230908b directory

--- tp4/sketch_230908b/pantalla_gano.js
+++
@@ -1,9 +0,0 @@
-function pantallaGano(){
-    background(0,255,0);
-  textAlign(CENTER,CENTER);
-  textSize(40);
-  fill(0);
-  text("GANASTE (:",width/2,height/2);
-  
-
-}
--- tp4/sketch_230908b/pantalla_perdio.js
+++
@@ -1,8 +0,0 @@
-function pantallaPerdio(){
-    background(255,0,0);
-  textAlign(CENTER,CENTER);
-  textSize(40);
-  fill(0);
-  text("PERDISTE ):",width/2,height/2);
-  
-}
