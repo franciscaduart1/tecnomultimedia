ff3bd2879eac01127cf184abaadde7b08e27e7ea
franciscaduart1 <129305147+franciscaduart1@users.noreply.github.com>
2023-10-17 16:30:58 -0300

    Add files via upload

---
+++ tp5/sketch_231012a/cerdito.js
@@ -0,0 +1,27 @@
+class Cerdito {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.velocidadX = random(1, 2); 
+    this.velocidadY = random(1, 2); 
+ //this.imagen = imagenCerdito; 
+ this.imagenCerdito = loadImage("data/imagess.jpg");
+  }
+
+  mostrar() {
+    image(this.imagenCerdito, this.x, this.y, 50, 50); 
+  }
+  mover() {
+    
+    this.x += this.velocidadX;
+    this.y += this.velocidadY;
+
+    // Hacer que los cerditos reboten 
+    if (this.x < 0 || this.x > width) {
+      this.velocidadX *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.velocidadY *= -1;
+    }
+  }
+}
---
+++ tp5/sketch_231012a/sketch_231012a.js
@@ -0,0 +1,22 @@
+//FRANCISCA DUARTE
+//COMISION 3-TPN5
+//VIDEO:https://youtu.be/DMBY9IrCP8U
+let juego;
+
+function setup() {
+  createCanvas(400, 400);
+  juego = new Juego();
+  juego.setup(); 
+}
+
+function draw() {
+  juego.dibujo(); 
+}
+
+function mousePressed() {
+  juego.mousePressed(); 
+}
+
+function keyPressed() {
+  juego.mover(); 
+}
